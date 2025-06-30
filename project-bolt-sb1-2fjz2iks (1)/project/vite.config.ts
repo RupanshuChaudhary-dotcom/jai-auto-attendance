124208cf461d4dc9fec909e984415ab4e068e36d
RupanshuChaudhary-dotcom <rupanshuj007@gmail.com>
2025-06-30 13:52:55 +0530

    Update vite.config.ts

--- project-bolt-sb1-2fjz2iks (1)/project/vite.config.ts
+++ project-bolt-sb1-2fjz2iks (1)/project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // 👈 change this
+  base: '/jai-auto-attendance/', // 👈 change this
   plugins: [react()],
 })
 });
